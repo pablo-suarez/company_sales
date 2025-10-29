73751ba6a3485a5ce119a76e9e96b452db0eaa42
pablo-suarez <jpsuarez2014@gmail.com>
2025-10-28 20:32:49 -0500

    Initial configuration - globalpipes & mongo

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+      ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRoot(
+      process.env.MONGODB_URI || 'mongodb://userAdmin:UserPassword@localhost:27017/',
+      {
+        retryWrites: true,
+        w: 'majority',
+      }
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
